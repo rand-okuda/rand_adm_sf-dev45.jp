14d34d9f9c442b6647d0cb6ec3762cc7439c531f
okuda <okuda@RD12PC071N>
2018-08-24 19:15:18 +0900

    PDFプレビュー機能微調整。

--- src/aura/InspectionPdfPreview/InspectionPdfPreviewController.js
+++ src/aura/InspectionPdfPreview/InspectionPdfPreviewController.js
@@ -6,7 +6,6 @@
         
         var urlEvent = $A.get("e.force:navigateToURL");
         
-        // URLを直接指定して呼び出します。
         urlEvent.setParams({"url":"/apex/InspectionTablePDF?id="+recordId});
         urlEvent.fire();
         
